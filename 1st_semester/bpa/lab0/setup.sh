b4bf4ebf8903db6f83557609553c09c7378c71d7
sexozavr <daniildiveev@mail.ru>
2022-09-14 22:58:28 +0300

    [ADD] added first two tasks for lab bpa 1

---
+++ 1st_semester/bpa/lab0/setup.sh
@@ -0,0 +1,54 @@
+mkdir beautifly3
+touch froslass6
+echo "Тип покемона ICE GHOST" > froslass6
+touch garchomp8
+echo "satk=8 sdef=9 \nspd=10" > garchomp8
+touch happiny1
+echo "Способности Last Chance Natural Cure Serene Grace" > happiny1
+mkdir rhydon0
+mkdir teddiursa5
+
+cd beautifly3
+touch alakazam
+echo "Способности Mind Mold Synchronize Inner \nFocus" > alakazam
+mkdir lillipup
+mkdir politoed
+touch darmanitan
+echo "Развитые способности Zen Mode" > darmanitan
+touch umbreon
+echo "Тип покемона \nDARK NONE" > umbreon
+
+cd .. 
+
+cd rhydon0
+mkdir vibrava
+mkdir dusclops
+mkdir glameow
+
+cd .. 
+
+cd teddiursa5
+mkdir deino
+mkdir rampardos
+mkdir venipede
+
+cd .. 
+tree
+
+chmod 524 beautifly3 
+chmod 004 beautifly3/alakazam
+chmod 752 beautifly3/lillipup
+chmod 737 beautifly3/politoed
+chmod 400 beautifly3/darmanitan
+chmod 400 beautifly3/umbreon
+chmod 404 froslass6
+chmod 640 garchomp8
+chmod 444 happiny1
+chmod 500 rhydon0
+chmod 737 rhydon0/vibrava
+chmod 335 rhydon0/dusclops
+chmod 550 rhydon0/glameow
+chmod 537 teddiursa5
+chmod 500 teddiursa5/deino
+chmod 777 teddiursa5/rampardos
+chmod 752 teddiursa5/venipede
