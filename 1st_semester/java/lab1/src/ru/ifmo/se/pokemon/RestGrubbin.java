03c9b6aca3c0f5aeb9a21fe3014f3e493ae1d07e
Daniil Diveev <daniildiveev@mail.ru>
2022-10-18 17:36:37 +0300

    [MODIFY] name fix in RestGrubbin

--- 1st_semester/java/lab1/src/ru/ifmo/se/pokemon/RestGrubbin.java
+++ 1st_semester/java/lab1/src/ru/ifmo/se/pokemon/RestGrubbin.java
@@ -2,7 +2,7 @@ package ru.ifmo.se.pokemon;
 import ru.ifmo.se.pokemon.StatusMove;
 import ru.ifmo.se.pokemon.Type;
 
-public class Rest extends StatusMove{
+public class RestGrubbin extends StatusMove{
     public Rest(){
         super(Type.PSYCHIC, 0, 0);
     }
