ebe5f0804ff29dafdceedb1b7007fed33ede3b3a
sexozavr <daniildiveev@mail.ru>
2022-10-18 14:08:21 +0300

    [MODIFY] add base code to Main.java

--- 1st_semester/java/lab1/src/ru/ifmo/se/pokemon/Main.java
+++ 1st_semester/java/lab1/src/ru/ifmo/se/pokemon/Main.java
@@ -4,5 +4,11 @@ import ru.ifmo.se.pokemon.Pokemon;
 
 class Main{
     public static void main(String[] args){
+        Battle b = new Battle();
+        Pokemon p1 = new Pokemon("Чужой", 1);
+        Pokemon p2 = new Pokemon("Хищник", 1);
+        b.addAlly(p1);
+        b.addFoe(p2);
+        b.go();
     }
 }
