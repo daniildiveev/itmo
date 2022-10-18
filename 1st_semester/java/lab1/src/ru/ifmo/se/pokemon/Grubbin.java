9a1b861572c2f9a859405fdbb7ee871dd353dcc4
Daniil Diveev <daniildiveev@mail.ru>
2022-10-18 17:28:51 +0300

    [MODIFY] added stats, type and moves for Grubbin

--- 1st_semester/java/lab1/src/ru/ifmo/se/pokemon/Grubbin.java
+++ 1st_semester/java/lab1/src/ru/ifmo/se/pokemon/Grubbin.java
@@ -1,7 +1,12 @@
 package ru.ifmo.se.pokemon;
+import ru.ifmo.se.pokemon.Type;
+import ru.ifmo.se.pokemon.Pokemon;
 
 class Grubbin extends Pokemon{
     public Grubbin(String name, int level){
         super(name, level);
+        setStats(47, 62, 45, 55, 45, 46);
+        setType(Type.BUG);
+        setMove(new RestGrubbin(), new Confide());
     }
 }
