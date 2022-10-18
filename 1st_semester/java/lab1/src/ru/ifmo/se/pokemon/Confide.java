4aa010a2b06af947d106d9d87cf067fa199a8ff3
Daniil Diveev <daniildiveev@mail.ru>
2022-10-18 17:28:04 +0300

    [ADD] implemented Confide and RestGrubbin move

---
+++ 1st_semester/java/lab1/src/ru/ifmo/se/pokemon/Confide.java
@@ -0,0 +1,18 @@
+package ru.ifmo.se.pokemon;
+import ru.ifmo.se.pokemon.StatusMove;
+
+public class Confide extends StatusMove{
+    public Confide(){
+        super(Type.NORMAL, 0, 0);
+    }
+
+    @Override
+    protected void applyOppEffects(Pokemon p){
+        p.setMod(Stat.SPECIAL_ATTACK, -1);
+    }
+
+    @Override
+    protected String describe(){
+        return "использует Confide";
+    }
+}
