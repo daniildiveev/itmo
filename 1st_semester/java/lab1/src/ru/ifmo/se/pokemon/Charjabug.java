6a41476cf65e3da4d490b41771bec20591f4cd83
Daniil Diveev <daniildiveev@mail.ru>
2022-10-18 17:49:15 +0300

    [MODIFY] added stats, types and moves for Charjabug

--- 1st_semester/java/lab1/src/ru/ifmo/se/pokemon/Charjabug.java
+++ 1st_semester/java/lab1/src/ru/ifmo/se/pokemon/Charjabug.java
@@ -1,7 +1,12 @@
 package ru.ifmo.se.pokemon;
+import ru.ifmo.se.pokemon.Type;
+import ru.ifmo.se.pokemon.Pokemon;
 
 class Charjabug extends Pokemon{
     public Charjabug(String name, int level){
         super(name, level);
+        setStats(57, 82, 95, 55, 75, 36);
+        setType(Type.BUG, Type.ELECTRIC);
+        setMove(new RestCharjabug(), new Crunch(), new Confide());
     }
 }
