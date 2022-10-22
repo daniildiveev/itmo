b294502bbb9f9d0f37c57bf844fc2c23f49c2f02
Daniil Diveev <daniildiveev@mail.ru>
2022-10-22 18:28:17 +0300

    [MODIFY] add stats, type and moves for Bergmite

--- 1st_semester/java/lab1/src/ru/ifmo/se/pokemon/Bergmite.java
+++ 1st_semester/java/lab1/src/ru/ifmo/se/pokemon/Bergmite.java
@@ -3,5 +3,8 @@ package ru.ifmo.se.pokemon;
 class Bergmite extends Pokemon{
     public Bergmite(String name, int level){
         super(name, level);
+        setStats(55, 69, 85, 32, 35, 28);
+        setType(Type.ICE);
+        setMove(new Blizzard(), new IceFang(), new FlashCannon());
     }
 }
