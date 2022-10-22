610098746a1396fb25269fa3953c54a7c02404d6
Daniil Diveev <daniildiveev@mail.ru>
2022-10-22 17:45:34 +0300

    [MODIFY] add moves for vikavolt

--- 1st_semester/java/lab1/src/ru/ifmo/se/pokemon/Vikavolt.java
+++ 1st_semester/java/lab1/src/ru/ifmo/se/pokemon/Vikavolt.java
@@ -1,7 +1,12 @@
 package ru.ifmo.se.pokemon;
+import ru.ifmo.se.pokemon.Type;
+import ru.ifmo.se.pokemon.Pokemon;
 
 class Vikavolt extends Pokemon{
     public Vikavolt(String name, int level){
         super(name, level);
+        setStats(77, 70, 90, 145, 75, 43);
+        setType(Type.BUG, Type.ELECTRIC);
+        setMove(new Confide(), new Crunch(), new RestVikavolt(), new AirSlash());
     }
 }
