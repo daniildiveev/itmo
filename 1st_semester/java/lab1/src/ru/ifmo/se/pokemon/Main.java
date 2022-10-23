95a7f30f9f0f126baba3eb29c6b582a2097e1860
Daniil Diveev <daniildiveev@mail.ru>
2022-10-23 15:48:27 +0300

    [MODIFY] implement the battle in Main

--- 1st_semester/java/lab1/src/ru/ifmo/se/pokemon/Main.java
+++ 1st_semester/java/lab1/src/ru/ifmo/se/pokemon/Main.java
@@ -3,10 +3,18 @@ package ru.ifmo.se.pokemon;
 class Main{
     public static void main(String[] args){
         Battle b = new Battle();
-        Pokemon p1 = new Pokemon("Чужой", 1);
-        Pokemon p2 = new Pokemon("Хищник", 1);
+        Terrakion p1 = new Terrakion("Terrakion", 1);
+        Bergmite p2 = new Bergmite("Bergmite", 1);
+        Avalugg p3 = new Avalugg("Avalugg", 1);
+        Grubbin p4 = new Grubbin("Grubbin", 1);
+        Charjabug p5 = new Charjabug("Charjabug", 1);
+        Vikavolt p6 = new Vikavolt("Vikavolt", 1);
         b.addAlly(p1);
         b.addFoe(p2);
+        b.addAlly(p3);
+        b.addFoe(p4);
+        b.addAlly(p5);
+        b.addFoe(p6);
         b.go();
     }
 }
