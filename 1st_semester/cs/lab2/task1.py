c8135c9c332a38376836bfe47d77999bc91ce291
sexozavr <daniildiveev@mail.ru>
2022-11-02 23:38:56 +0300

    [MODIFY] add python tests for task 1 cs lab2

--- 1st_semester/cs/lab2/task1.py
+++ 1st_semester/cs/lab2/task1.py
@@ -1,5 +1,6 @@
 import re 
 
+EMOJI = ":<|"
 EMOJI_REGEXP = r":<\|"
 TEST_CORPUS = ("^%&$*&^%&^$%:<|>:<|",
                ":<<<|>|:<|",
@@ -9,4 +10,5 @@ TEST_CORPUS = ("^%&$*&^%&^$%:<|>:<|",
 
 if __name__ == "__main__":
     for test_string in TEST_CORPUS:
-        print("Number of emojis: %s" % len(re.findall(EMOJI_REGEXP,test_string)))
+        print("Number of emojis found by regex: %s" % len(re.findall(EMOJI_REGEXP,test_string)))
+        print("Number of emojis found by python: %s" % test_string.count(":<|"))
