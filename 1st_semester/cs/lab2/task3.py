dd94eba04a844fbacbbf2f95a309d7450b708f46
Daniil Diveev <daniildiveev@mail.ru>
2022-11-02 21:08:10 +0300

    [MODIFY] add tests for task3 cs lab2

--- 1st_semester/cs/lab2/task3.py
+++ 1st_semester/cs/lab2/task3.py
@@ -1,18 +1,24 @@
 import re
 
-TEST_STRING = "Классное слово – обороноспособность, которое должно идти после слов: трава и молоко."
+TEST_CORPUS = ("Классное слово – обороноспособность, которое должно идти после слов: трава и молоко.",
+               "Олово золото красит",
+               "Хорошо жить в мире, а не в войне", 
+               "Волчок и юла крутятся", 
+               "Быть или не быть: вот в чем вопрос")
 
 if __name__ == "__main__":
-    test_string = re.findall(r"\w+", TEST_STRING)
-    test_string = " ".join(test_string).lower()
-    words = test_string.split()
-    matching_words = []
 
-    for word in words:
-        vowels = set(re.findall(r"[аяуеыоэюи]+", word))
+    for test_string in TEST_CORPUS:
+        test_string = re.findall(r"\w+", test_string)
+        test_string = " ".join(test_string).lower()
+        words = test_string.split()
+        matching_words = []
 
-        if len(vowels) == 1:
-            matching_words.append(word)
+        for word in words:
+            vowels = set(re.findall(r"[аяуеыоэюи]+", word))
 
-    print(*sorted(matching_words, key=lambda x: len(x)))
- 
+            if len(vowels) == 1:
+                matching_words.append(word)
+
+        print(*sorted(matching_words, key=lambda x: len(x)))
+    
