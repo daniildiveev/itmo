5c71577bdd549eb4053e2844dfa8b52b70a67d2c
Daniil Diveev <daniildiveev@mail.ru>
2022-11-03 21:50:26 +0300

    [MODIFY] wrapped parsing with libs process into a function

--- 1st_semester/cs/lab3/parsing_with_libs.py
+++ 1st_semester/cs/lab3/parsing_with_libs.py
@@ -4,11 +4,15 @@ from dict2xml import dict2xml
 SOURCE_FILE = "timetable.json"
 OUTPUT_FILE = "timetable.xml"
 
-if __name__ == "__main__":
-    with open(SOURCE_FILE) as f:
+def parse_json_and_dump_to_xml(source_file:str,
+                               output_file:str) -> None:
+    with open(source_file) as f:
         data = json.load(f)
     
     xml_code = dict2xml(data)
 
-    with open(OUTPUT_FILE, 'w') as f:
+    with open(output_file, 'w') as f:
         f.write(xml_code)
+
+if __name__ == "__main__":
+    parse_json_and_dump_to_xml(SOURCE_FILE, OUTPUT_FILE)
