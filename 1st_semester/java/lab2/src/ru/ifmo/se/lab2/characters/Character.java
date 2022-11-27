028e8349ae4da23af5fb63eb1b156862bb90f7c5
Daniil Diveev <daniildiveev@mail.ru>
2022-11-27 21:58:56 +0300

    [MODIFY] override method tostring in character class

--- 1st_semester/java/lab2/src/ru/ifmo/se/lab2/characters/Character.java
+++ 1st_semester/java/lab2/src/ru/ifmo/se/lab2/characters/Character.java
@@ -5,4 +5,9 @@ public abstract class Character{
     public String carry(Carriable с){
         return "приносить " + с.toString();
     }
+
+    @Override
+    public String toString(){
+        return "все "
+    }
 }
