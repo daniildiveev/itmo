e406bdc117484ae7a135e71c8deb9f8b4c9f53f5
Daniil Diveev <daniildiveev@mail.ru>
2022-11-27 16:53:26 +0300

    [ADD] add file for abstract class Madeable;

---
+++ 1st_semester/java/lab2/src/ru/ifmo/se/lab2/objects/Madeable.java
@@ -0,0 +1,13 @@
+package ru.ifmo.se.lab2.objects;
+
+public abstract class Madeable{
+    public static String material;
+
+    public Madeable(Mineable m){
+        material = m.toString();    
+    }
+
+    public String getMaterial(){
+        return "из " + material;
+    }
+} 
