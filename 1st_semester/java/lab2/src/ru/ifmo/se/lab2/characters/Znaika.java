cf1300d4667caf4374f7771cca8827225ee5a8a3
Daniil Diveev <daniildiveev@mail.ru>
2022-11-27 16:48:09 +0300

    [MODIFY] implement methods think, getTheIdea and override tostring method

--- 1st_semester/java/lab2/src/ru/ifmo/se/lab2/characters/Znaika.java
+++ 1st_semester/java/lab2/src/ru/ifmo/se/lab2/characters/Znaika.java
@@ -1,7 +1,29 @@
 package ru.ifmo.se.lab2.characters;
+import ru.ifmo.se.lab2.enums.PartOfDay;
+import ru.ifmo.se.lab2.objects.Madeable;
 
-class Znaika extends Character{
-    public void think(){
-        System.out.println("Стал думать");
+public class Znaika extends Character{
+    public String think(PartOfDay p, int howLong){
+        String output = "думал ";
+
+        if (howLong != 0){
+            output = output + Integer.toString(howLong);
+
+            switch (p) {
+            case NIGHT -> output = output + " ночи ";
+            case DAY -> output = output + " дня "; 
+            }
+        }
+
+        return output;
+    }
+
+    @Override
+    public String toString(){
+        return "Знайка ";
+    }
+
+    public String getTheIdea(Madeable m){
+        return "придумал " + m.toString() + m.getMaterial();
     }
 }
