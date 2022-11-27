82ecfd13bee2132ea45e75373097bc81929bf056
Daniil Diveev <daniildiveev@mail.ru>
2022-11-27 20:50:20 +0300

    [MODIFY] transform, thickify and transform methods

--- 1st_semester/java/lab2/src/ru/ifmo/se/lab2/objects/Juice.java
+++ 1st_semester/java/lab2/src/ru/ifmo/se/lab2/objects/Juice.java
@@ -1,8 +1,22 @@
 package ru.ifmo.se.lab2.objects;
 
 public class Juice implements Gatherable, Carriable{
+    public final String color = " белый";
+    
     @Override
     public String toString(){
         return "сок ";
+    }
+
+    public String dripOut(){
+        return " вытекает ";
+    }
+
+    public String thickify(){
+        return " густеет ";
+    }
+
+    public String transform(Mineable m){
+        return "превращается в " + m.toString();
     }
 }
