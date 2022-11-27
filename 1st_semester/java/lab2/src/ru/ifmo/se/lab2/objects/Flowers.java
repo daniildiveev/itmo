5de0368e57d7cdf982c5ab589cca7f264f3859c8
Daniil Diveev <daniildiveev@mail.ru>
2022-11-27 20:53:47 +0300

    [MODIFY] add class for flowers, implement tostring, grow, lookalike, makeacut methods and inner class Stem

---
+++ 1st_semester/java/lab2/src/ru/ifmo/se/lab2/objects/Flowers.java
@@ -0,0 +1,36 @@
+package ru.ifmo.se.lab2.objects;
+import ru.ifmo.se.lab2.enums.Place;
+
+public class Flowers{
+    class Stem{
+        @Override
+        public String toString(){
+            return " стебель ";
+        }
+    }
+
+    final Stem stem = new Stem(); 
+
+    public String grow(Place p){
+        if (p == Place.CITY){
+            return "В городе росли";
+        }
+
+        else {
+            return "";
+        }
+    }
+
+    @Override
+    public String toString(){
+        return " цветы ";
+    }
+
+    public String lookALike(){
+        return " похожие на фикусы ";
+    }
+
+    public String makeACut(){
+        return "сделать надрез " + stem.toString();
+    }
+}
