8e64bb823f34b97562cb2030434a899adfffce4d
Daniil Diveev <daniildiveev@mail.ru>
2022-11-27 23:00:12 +0300

    [RENAME] rename character to person

--- 1st_semester/java/lab2/src/ru/ifmo/se/lab2/characters/Person.java
+++ 1st_semester/java/lab2/src/ru/ifmo/se/lab2/characters/Person.java
@@ -1,13 +1,13 @@
 package ru.ifmo.se.lab2.characters;
 import ru.ifmo.se.lab2.objects.Carriable;
 
-public abstract class Character{ 
+public class Person{ 
     public String carry(Carriable с){
         return "приносить " + с.toString();
     }
 
     @Override
     public String toString(){
-        return "все "
+        return "все ";
     }
 }
