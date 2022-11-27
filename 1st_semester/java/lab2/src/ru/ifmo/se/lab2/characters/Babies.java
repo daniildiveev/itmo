a61eff177a288b0f66669570bc6802a01ac288af
Daniil Diveev <daniildiveev@mail.ru>
2022-11-27 22:59:17 +0300

    [MODIFY] add overriden tostring method to babies class

--- 1st_semester/java/lab2/src/ru/ifmo/se/lab2/characters/Babies.java
+++ 1st_semester/java/lab2/src/ru/ifmo/se/lab2/characters/Babies.java
@@ -1,5 +1,8 @@
 package ru.ifmo.se.lab2.characters;
 
-public class Babies extends Character{
-    
+public class Babies extends Person{
+    @Override
+    public String toString(){
+        return " малышки";
+    }
 }
