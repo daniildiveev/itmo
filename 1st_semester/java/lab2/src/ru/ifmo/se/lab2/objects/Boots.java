1d7b1d6f0cc02c597080e31f46b26aae1348207c
Daniil Diveev <daniildiveev@mail.ru>
2022-11-27 20:52:14 +0300

    [ADD] add class for boots and override tostring method

---
+++ 1st_semester/java/lab2/src/ru/ifmo/se/lab2/objects/Boots.java
@@ -0,0 +1,12 @@
+package ru.ifmo.se.lab2.objects;
+
+public class Boots extends Madeable{
+    public Boots(Mineable m){
+        super(m);
+    }
+
+    @Override
+    public String toString(){
+        return " калоши ";
+    }
+}
