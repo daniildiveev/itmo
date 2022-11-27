f38a06193f81b9b7918ec9e52b1b8e3b3781659a
Daniil Diveev <daniildiveev@mail.ru>
2022-11-27 20:42:49 +0300

    [MODIFY] changed the order of compiling

--- 1st_semester/java/lab2/build/compile_and_create_jar.sh
+++ 1st_semester/java/lab2/build/compile_and_create_jar.sh
@@ -1,7 +1,7 @@
-javac -cp ".:../src" -d classes/ ../src/ru/ifmo/se/lab2/*.java
-javac -d classes/ ../src/ru/ifmo/se/lab2/objects/*
-javac -cp ".:../src" -d classes/ ../src/ru/ifmo/se/lab2/characters/*
 javac -d classes/ ../src/ru/ifmo/se/lab2/enums/*
+javac -cp ".:../src" -d classes/ ../src/ru/ifmo/se/lab2/*.java
+javac -cp ".:../src" -d classes/ ../src/ru/ifmo/se/lab2/objects/*
+javac -cp ".:../src" -d classes/ ../src/ru/ifmo/se/lab2/characters/*
 
 cd classes 
 java ru.ifmo.se.lab2.Main
