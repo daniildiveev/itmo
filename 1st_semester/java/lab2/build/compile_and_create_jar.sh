9d99d601da665e087014a35aba526d1763136f3d
Daniil Diveev <daniildiveev@mail.ru>
2022-11-27 16:45:01 +0300

    [MODIFY] add commands for packages compiling

--- 1st_semester/java/lab2/build/compile_and_create_jar.sh
+++ 1st_semester/java/lab2/build/compile_and_create_jar.sh
@@ -1,2 +1,8 @@
+javac -cp ".:../src" -d classes/ ../src/ru/ifmo/se/lab2/*.java
 javac -d classes/ ../src/ru/ifmo/se/lab2/objects/*
-javac -d classes/  ../src/ru/ifmo/se/lab2/characters/*
+javac -cp ".:../src" -d classes/ ../src/ru/ifmo/se/lab2/characters/*
+javac -d classes/ ../src/ru/ifmo/se/lab2/enums/*
+
+cd classes 
+java ru.ifmo.se.lab2.Main
+cd .. 
