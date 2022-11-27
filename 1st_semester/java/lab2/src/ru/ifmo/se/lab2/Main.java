740f36c5a4e7cadcdc6015cb1022f06513818c4f
Daniil Diveev <daniildiveev@mail.ru>
2022-11-27 20:43:42 +0300

    [MODIFY] add 2 more sentences

--- 1st_semester/java/lab2/src/ru/ifmo/se/lab2/Main.java
+++ 1st_semester/java/lab2/src/ru/ifmo/se/lab2/Main.java
@@ -9,6 +9,9 @@ class Main {
         Shorties s = new Shorties();
         Rubber rubber = new Rubber();
         Sphere sphere = new Sphere(rubber);
+        Boots boots = new Boots(rubber);
+        Flowers flowers = new Flowers();
+        Juice j = new Juice();
 
         String output = z.toString() + z.think(PartOfDay.NONE, 0);
         output = output + z.toString() + z.think(PartOfDay.DAY, 3);
@@ -16,6 +19,11 @@ class Main {
         output = output + z.getTheIdea(sphere);
 
         output = output + s.toString() + s.gather(rubber);
+
+        output = output + flowers.grow(Place.CITY) + flowers.toString() + flowers.lookALike();
+        output = output + flowers.makeACut() + j.dripOut() + j.color + j.toString();
+        output = output + j.toString() + j.thickify() + j.transform(rubber);
+        output = output + boots.getMaterial() + boots.canBeMadeOutOf() + sphere.canBeMadeOutOf();
 
         System.out.println(output);
     }
