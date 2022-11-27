e0dbcc00b5b291c8bbe35b61af56801beb90a52f
Daniil Diveev <daniildiveev@mail.ru>
2022-11-27 23:03:55 +0300

    [ADD] add gunka class, override tostring and implement play method

---
+++ 1st_semester/java/lab2/src/ru/ifmo/se/lab2/characters/Gunka.java
@@ -0,0 +1,12 @@
+package ru.ifmo.se.lab2.characters;
+
+public class Gunka extends Person{
+    @Override
+    public String toString(){
+        return " Гунька";
+    }
+
+    public String playPrigalki(Person p, int amountOfPlayers){
+        return " играть в прыгалки с " + Integer.toString(amountOfPlayers) + p.toString();
+    }
+}
