4c24c10f9ebe142f970a230eeef8bec9f37230d2
Daniil Diveev <daniildiveev@mail.ru>
2022-11-27 23:01:16 +0300

    [MODIFY] add constructor and meet method to NeZnaika class

--- 1st_semester/java/lab2/src/ru/ifmo/se/lab2/characters/NeZnaika.java
+++ 1st_semester/java/lab2/src/ru/ifmo/se/lab2/characters/NeZnaika.java
@@ -1,5 +1,12 @@
 package ru.ifmo.se.lab2.characters;
 
-public class NeZnaika extends Character{
-    
+public class NeZnaika extends Person{
+    @Override
+    public String toString(){
+        return " НеЗнайка";
+    }
+
+    public String meet(Person p){
+        return " встретил" + p.toString();
+    }
 }
