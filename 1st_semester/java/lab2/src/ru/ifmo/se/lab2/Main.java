eeb85b3555602fae3e3895b47c259328da2e5b16
Daniil Diveev <daniildiveev@mail.ru>
2022-11-28 20:41:27 +0300

    [MODIFY] changed everything with respect to other structure changes

--- 1st_semester/java/lab2/src/ru/ifmo/se/lab2/Main.java
+++ 1st_semester/java/lab2/src/ru/ifmo/se/lab2/Main.java
@@ -16,24 +16,22 @@ class Main {
         Flowers flowers = new Flowers();
         Juice juice = new Juice();
         Ball ball = new Ball(rubber);
-        Person character = new Person();
+        Person person = new Person();
         Barrel barrel = new Barrel();
 
         String output = z.toString() + z.think(PartOfDay.NONE, 0);
         output = output + z.toString() + z.think(PartOfDay.DAY, 3);
         output = output + z.think(PartOfDay.NIGHT, 3);
         output = output + z.getTheIdea() + sphere.toString() + sphere.getMaterial();
-
-        output = output + s.toString() + s.gather(rubber);
-
+        output = output + s.toString() + s.mine(rubber);
         output = output + flowers.grow(Place.CITY) + flowers.toString() + flowers.lookALike();
-        output = output + flowers.makeACut() + juice.dripOut() + juice.color + juice.toString();
+        output = output + flowers.makeACut() + juice.dripOut() + juice.specifyType(JuiceParams.WHITE) + juice.toString();
         output = output + juice.toString() + juice.thickify() + juice.transform(rubber);
         output = output + boots.getMaterial() + boots.canBeMadeOutOf() + ball.canBeMadeOutOf();
-        output = output + z.toString() + z.getTheIdea() + z.toString() + z.tell(s);
-        output = output + s.gather(juice) + character.toString() + character.carry(juice);
+        output = output + z.toString() + z.getTheIdea() + z.toString() + z.tell(b);
+        output = output + b.gather(juice) + juice.specifyType(JuiceParams.RUBBER) + person.toString() + person.carry(juice);
         output = output + z.toString() + z.prepare(barrel);
-        output = output + nz.toString() + nz.meet(g) + g.playPrigalki(b, 2); 
+        output = output + nz.toString() + nz.meet(g, Place.STREET) + g.playPrigalki(b, 2); 
 
         System.out.println(output);
     }
