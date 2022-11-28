9235cbd725eabbd4c4c6541ebe07169341225bbb
Daniil Diveev <daniildiveev@mail.ru>
2022-11-28 20:28:28 +0300

    [MODIFY] fix import

--- 1st_semester/java/lab2/src/ru/ifmo/se/lab2/characters/Shorties.java
+++ 1st_semester/java/lab2/src/ru/ifmo/se/lab2/characters/Shorties.java
@@ -1,11 +1,11 @@
 package ru.ifmo.se.lab2.characters;
-import ru.ifmo.se.lab2.objects.*;
+import ru.ifmo.se.lab2.objects.Mineable;
 
 public class Shorties extends Person{
     public String mine(Mineable m){
         return "добывать " + m.toString();
     }
-    
+
     @Override
     public String toString(){
         return "Коротышки ";
