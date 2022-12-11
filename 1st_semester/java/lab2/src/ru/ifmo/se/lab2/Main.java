2e2a72b69b7e4608738c4dbbfb549658d2b12154
Daniil Diveev <daniildiveev@mail.ru>
2022-12-11 21:49:24 +0300

    [MODIFY] minor fixes with respect to Thnkable interface

--- 1st_semester/java/lab2/src/ru/ifmo/se/lab2/Main.java
+++ 1st_semester/java/lab2/src/ru/ifmo/se/lab2/Main.java
@@ -22,13 +22,13 @@ class Main {
         String output = z.toString() + z.think(PartOfDay.NONE, 0);
         output = output + z.toString() + z.think(PartOfDay.DAY, 3);
         output = output + z.think(PartOfDay.NIGHT, 3);
-        output = output + z.getTheIdea() + sphere.toString() + sphere.getMaterial();
+        output = output + z.getTheIdea(sphere) + sphere.getMaterial() + sphere.getMaterial();
         output = output + s.toString() + s.mine(rubber);
         output = output + flowers.grow(Place.CITY) + flowers.toString() + flowers.lookALike();
         output = output + flowers.makeACut() + juice.dripOut() + juice.specifyType(JuiceParams.WHITE) + juice.toString();
         output = output + juice.toString() + juice.thickify() + juice.transform(rubber);
         output = output + boots.getMaterial() + boots.canBeMadeOutOf() + ball.canBeMadeOutOf();
-        output = output + z.toString() + z.getTheIdea() + z.toString() + z.tell(b);
+        output = output + z.toString() + z.getTheIdea(ball) + z.getTheIdea(boots) + z.toString() + z.tell(b);
         output = output + b.gather(juice) + juice.specifyType(JuiceParams.RUBBER) + person.toString() + person.carry(juice);
         output = output + z.toString() + z.prepare(barrel);
         output = output + nz.toString() + nz.meet(g, Place.STREET) + g.playPrigalki(b, 2); 
