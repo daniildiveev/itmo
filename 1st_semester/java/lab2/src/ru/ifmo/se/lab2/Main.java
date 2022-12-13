bdfc966dbaa0ecb3eb7ff86198cb161588978c82
Daniil Diveev <daniildiveev@mail.ru>
2022-12-13 23:20:16 +0300

    [MODIFY] get rid out of using toString right in Main

--- 1st_semester/java/lab2/src/ru/ifmo/se/lab2/Main.java
+++ 1st_semester/java/lab2/src/ru/ifmo/se/lab2/Main.java
@@ -19,19 +19,18 @@ class Main {
         Person person = new Person();
         Barrel barrel = new Barrel();
 
-        String output = z.toString() + z.think(PartOfDay.NONE, 0);
-        output = output + z.toString() + z.think(PartOfDay.DAY, 3);
-        output = output + z.think(PartOfDay.NIGHT, 3);
-        output = output + z.getTheIdea(sphere) + sphere.getMaterial() + sphere.getMaterial();
+        String output = z.think(PartOfDay.NONE, 0) + z.think(PartOfDay.DAY, 3) + z.think(PartOfDay.NIGHT, 3);
+        output = output + z.getTheIdea(sphere) + sphere.getMaterial();
         output = output + s.toString() + s.mine(rubber);
         output = output + flowers.grow(Place.CITY) + flowers.toString() + flowers.lookALike();
         output = output + flowers.makeACut() + juice.dripOut() + juice.specifyType(JuiceParams.WHITE) + juice.toString();
-        output = output + juice.toString() + juice.thickify() + juice.transform(rubber);
+        output = output + juice.toString() + juice.thicken() + juice.transform(rubber);
         output = output + boots.getMaterial() + boots.canBeMadeOutOf() + ball.canBeMadeOutOf();
-        output = output + z.toString() + z.getTheIdea(ball) + z.getTheIdea(boots) + z.toString() + z.tell(b);
-        output = output + b.gather(juice) + juice.specifyType(JuiceParams.RUBBER) + person.toString() + person.carry(juice);
-        output = output + z.toString() + z.prepare(barrel);
-        output = output + nz.toString() + nz.meet(g, Place.STREET) + g.playPrigalki(b, 2); 
+        output = output + z.getTheIdea(ball) + z.getTheIdea(boots) + z.tell(b);
+        output = output + b.gather(juice) + juice.specifyType(JuiceParams.RUBBER) +  person.carry(juice);
+        output = output + z.prepare(barrel);
+        output = output + nz.gather(juice);
+        output = output + nz.meet(g, Place.STREET) + g.playPrigalki(b, 2); 
 
         System.out.println(output);
     }
