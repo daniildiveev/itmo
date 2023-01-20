e56af2a741b522e4c7d9cc8ab82bcd90de2628d3
Daniil Diveev <daniildiveev@mail.ru>
2023-01-20 18:16:57 +0300

    [ADD] add class for person with emotions

---
+++ 1st_semester/java/lab3/src/ru/ifmo/se/lab3/characters/PersonWithEmotions.java
@@ -0,0 +1,35 @@
+package ru.ifmo.se.lab3.characters;
+import ru.ifmo.se.lab3.enums.Emotions;
+
+public class PersonWithEmotions extends Person{
+    private String emotion;
+
+    public String expressEmotion(Emotion e){
+        switch (e){
+            case INTERESTING -> this.emotion = " интересно";
+            case JEALOUS -> this.emotion = " завидно";
+            default -> this.emotion = ""; 
+        }
+    }
+
+    public String getEmotion(){
+        return this.emotion;
+    }
+
+    @Override
+    public boolean equals(Object o){
+        if (this.getClass() != o.getClass() || o == null){
+            return false;
+        }
+
+        PersonWithEmotions pwe = (PersonWithEmotions) o;
+
+        return this.emotion == pwe.getEmotion();
+    }
+
+    @Override
+    public int hashCode(){
+        String stringToHash = this.getClass().getSimpleName() + this.emotion;
+        return stringToHash.hashCode();
+    }
+}
