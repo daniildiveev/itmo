48e75daa9084009e542caa686c8c3e6542e40a8b
Daniil Diveev <daniildiveev@mail.ru>
2023-01-23 18:10:42 +0300

    [MODIFY] add classes and jar deletion before compiling code

--- 1st_semester/java/lab3/build/compile_and_create_jar.sh
+++ 1st_semester/java/lab3/build/compile_and_create_jar.sh
@@ -1,3 +1,5 @@
+rm -rf classes/ lab3.jar 2>/dev/null
+
 javac -d classes/ ../src/ru/ifmo/se/lab3/enums/*
 javac -d classes/ ../src/ru/ifmo/se/lab3/exceptions/*
 javac -cp ".:../src" -d classes/ ../src/ru/ifmo/se/lab3/*.java
