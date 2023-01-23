31ddabff048170877db8ddd4ab310eb3bc3500bb
Daniil Diveev <daniildiveev@mail.ru>
2023-01-24 01:25:38 +0300

    [MODIFY] change the order of compiling

--- 1st_semester/java/lab3/build/compile_and_create_jar.sh
+++ 1st_semester/java/lab3/build/compile_and_create_jar.sh
@@ -2,9 +2,9 @@ rm -rf classes/ lab3.jar 2>/dev/null
 
 javac -d classes/ ../src/ru/ifmo/se/lab3/enums/*
 javac -d classes/ ../src/ru/ifmo/se/lab3/exceptions/*
-javac -cp ".:../src" -d classes/ ../src/ru/ifmo/se/lab3/*.java
 javac -cp ".:../src" -d classes/ ../src/ru/ifmo/se/lab3/objects/*
 javac -cp ".:../src" -d classes/ ../src/ru/ifmo/se/lab3/characters/*
+javac -cp ".:../src" -d classes/ ../src/ru/ifmo/se/lab3/*.java
 
 jar cmf MANIFEST.MF lab3.jar -C classes/ .
 
