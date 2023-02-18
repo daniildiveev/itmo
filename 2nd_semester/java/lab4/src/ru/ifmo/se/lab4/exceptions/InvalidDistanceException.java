fd1ac16703e3611c895e38a2c8c5ab16073fc7c8
sexozavr <daniildiveev@mail.ru>
2023-02-18 23:08:11 +0300

    [ADD] add InvalidDistanceException exception

---
+++ 2nd_semester/java/lab4/src/ru/ifmo/se/lab4/exceptions/InvalidDistanceException.java
@@ -0,0 +1,7 @@
+package ru.ifmo.se.lab4.exceptions;
+
+public class InvalidDistanceException extends Exception{
+    public InvalidDistanceException(String message){
+        super(message);
+    }
+}
