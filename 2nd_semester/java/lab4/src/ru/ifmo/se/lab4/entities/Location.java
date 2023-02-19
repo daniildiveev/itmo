6370c0fabaf16f1f700622318e7b3a9961ef0774
sexozavr <daniildiveev@mail.ru>
2023-02-19 15:27:03 +0300

    [MODIFY] implement first version of constructor for Location

--- 2nd_semester/java/lab4/src/ru/ifmo/se/lab4/entities/Location.java
+++ 2nd_semester/java/lab4/src/ru/ifmo/se/lab4/entities/Location.java
@@ -1,8 +1,38 @@
 package ru.ifmo.se.lab4.entities;
 
-public class Location {
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Location{
     private Integer x; //Поле не может быть null
     private float y;
     private double z;
     private String name; //Строка не может быть пустой, Поле не может быть null
+    private BufferedReader reader;
+
+    public Location(){
+        System.out.println("Input now for parameters of Location class..");
+        BufferedReader this.reader = new BufferedReader(new InputStreamReader(System.in));
+    }
+
+    private void inputX(){
+        System.out.println("Please input the x parameter of Location");
+
+        try{
+            String input = this.reader.readLine();
+
+            if(input == null){
+                System.out.println("x parameter cannot be null");
+                inputX();
+            }
+
+            this.x = Integer.parseInt(input);
+        }
+
+        catch (Exception e){
+            System.out.println("Invalid x value, please try again...");
+            inputX();
+        }
+    }
 }
