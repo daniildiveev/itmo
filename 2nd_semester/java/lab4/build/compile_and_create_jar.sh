1e12ca4402750402c9b3a12c3400e3c0f72ef6eb
sexozavr <daniildiveev@mail.ru>
2023-02-19 15:25:56 +0300

    [MODIFY] add handlers and Main compilation to cacj file

--- 2nd_semester/java/lab4/build/compile_and_create_jar.sh
+++ 2nd_semester/java/lab4/build/compile_and_create_jar.sh
@@ -2,6 +2,8 @@ rm -rf classes/ lab4.jar 2>/dev/null
 
 javac -d classes/ ../src/ru/ifmo/se/lab4/exceptions/*
 javac -cp ".:../src" -d classes/ ../src/ru/ifmo/se/lab4/entities/*
+javac -cp ".:../src" -d classes/ ../src/ru/ifmo/se/lab4/handler/*
+javac -cp ".:../src" -d classes/ ../src/ru/ifmo/se/lab4/*.java
 
 jar -cvfm lab4.jar MANIFEST.MF -C classes/ .
 java -jar lab4.jar
