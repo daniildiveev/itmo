3ce3ad492e11221caa666b5ea61a163c0a5d978a
sexozavr <daniildiveev@mail.ru>
2023-02-24 01:15:51 +0300

    [MODIFY] add commands compilation, delete running jar

--- 2nd_semester/java/lab4/build/compile_and_create_jar.sh
+++ 2nd_semester/java/lab4/build/compile_and_create_jar.sh
@@ -1,9 +1,9 @@
 rm -rf classes/ lab4.jar 2>/dev/null
 
+javac -d classes/ ../src/ru/ifmo/se/lab4/handler/*
 javac -d classes/ ../src/ru/ifmo/se/lab4/exceptions/*
 javac -cp ".:../src" -d classes/ ../src/ru/ifmo/se/lab4/entities/*
-javac -cp ".:../src" -d classes/ ../src/ru/ifmo/se/lab4/handler/*
+javac -cp ".:../src" -d classes/ ../src/ru/ifmo/se/lab4/commands/*
 javac -cp ".:../src" -d classes/ ../src/ru/ifmo/se/lab4/*.java
 
 jar -cvfm lab4.jar MANIFEST.MF -C classes/ .
-java -jar lab4.jar
