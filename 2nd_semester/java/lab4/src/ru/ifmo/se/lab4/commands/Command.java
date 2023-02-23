faa0741b88820abd8ea047a3b8f3ae9d9200596e
sexozavr <daniildiveev@mail.ru>
2023-02-23 21:50:20 +0300

    [ADD] fix types in Command

--- 2nd_semester/java/lab4/src/ru/ifmo/se/lab4/commands/Command.java
+++ 2nd_semester/java/lab4/src/ru/ifmo/se/lab4/commands/Command.java
@@ -1,7 +1,7 @@
 package ru.ifmo.se.lab4.commands;
 
 public interface Command{
-    void getName();
-    void getDescription();
+    String getName();
+    String getDescription();
     void execute();
 }
