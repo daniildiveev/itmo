a8ecf06ba99fda91cc92812f2a8b93e37510742d
sexozavr <daniildiveev@mail.ru>
2023-02-24 16:49:06 +0300

    [MODIFY] implement Route constructor the right way

--- 2nd_semester/java/lab4/src/ru/ifmo/se/lab4/entities/Route.java
+++ 2nd_semester/java/lab4/src/ru/ifmo/se/lab4/entities/Route.java
@@ -1,9 +1,15 @@
 package ru.ifmo.se.lab4.entities;
 
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.time.LocalDateTime;
+
 import ru.ifmo.se.lab4.exceptions.*;
+import ru.ifmo.se.lab4.handler.IOHandler;
 
 public class Route implements Comparable<Route>{
+    private static int id_next=1;
     private int id; //Значение поля должно быть больше 0, Значение этого поля должно быть уникальным, Значение этого поля должно генерироваться автоматически
     private String name; //Поле не может быть null, Строка не может быть пустой
     private Coordinates coordinates; //Поле не может быть null
@@ -11,32 +17,63 @@ public class Route implements Comparable<Route>{
     private Location from; //Поле может быть null
     private Location to; //Поле не может быть null
     private long distance; //Значение поля должно быть больше 1
+    private BufferedReader reader;
 
-    public Route(String name, Coordinates coordinates, Location from, Location to, long distance)
-        throws Exception{
-        //TODO: implement id setting
-        if (name == null || name.equals("")){
-            throw new InvalidNameException("invalid Route name");
-        }
+    public Route() throws Exception{
+        IOHandler.println("Input now parameters for Route class..");
+        this.reader = new BufferedReader(new InputStreamReader(System.in));
 
-        if (coordinates == null){
-            throw new NoCoordinatesException("no coordinates provided");
-        }        
+        this.id = id_next;
+        id_next++;
 
-        if (from == null || to == null){
-            throw new NoLocationException("no location provided");
-        }
+        inputName();
 
-        if (distance <= 1){
-            throw new InvalidDistanceException("invalid distance provided");
-        }
-
-        this.name = name;
-        this.coordinates = coordinates;
+        this.coordinates = new Coordinates();
         this.creationDate = LocalDateTime.now();
-        this.from = from;
-        this.to = to;
-        this.distance = distance;
+        this.from = new Location();
+        this.to = new Location();
+
+        inputDistance();
+    }
+
+    private void inputName(){
+        IOHandler.print("Please input the name parameter of Route >>");
+
+        try{
+            String input = this.reader.readLine();
+
+            if (input.equals("") || input == null){
+                throw new InvalidNameException("name parameter cannot be null");
+            }
+
+            if (this.name == null){
+                this.name = input;
+            }
+        }
+
+        catch (Exception e){
+            IOHandler.println(e.getMessage());
+            IOHandler.println("Invalid name value, please try again...");
+            inputName();
+        }
+    }
+
+    private void inputDistance(){
+        IOHandler.print("Please input the distance parameter of Route >>");
+
+        try{
+            String input = this.reader.readLine();
+
+            if (Long.parseLong(input) <= 1){
+                throw new InvalidDistanceException("distance must be greater than 1");
+            }
+        }
+
+        catch (Exception e){
+            IOHandler.println(e.getMessage());
+            IOHandler.println("Invalid distance provided, please try again...");
+            inputDistance();
+        }
     }
 
     public int compareTo(Route r){
