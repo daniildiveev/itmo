2781ab16f8769a6d3456ceea8791a39e0c2a5891
sexozavr <daniildiveev@mail.ru>
2023-03-16 18:48:23 +0300

    [MODIFY] fix signature in command interface

--- 2nd_semester/java/lab4/src/ru/ifmo/se/lab4/commands/Command.java
+++ 2nd_semester/java/lab4/src/ru/ifmo/se/lab4/commands/Command.java
@@ -1,7 +1,10 @@
 package ru.ifmo.se.lab4.commands;
 
+import java.util.PriorityQueue;
+import ru.ifmo.se.lab4.entities.Route;
+
 public interface Command{
     String getName();
     String getDescription();
-    void execute();
+    void execute(PriorityQueue<Route> collection, String[] args);
 }
