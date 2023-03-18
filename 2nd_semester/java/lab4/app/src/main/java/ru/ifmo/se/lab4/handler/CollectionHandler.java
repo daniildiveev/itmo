6b112fb44c418abfdfe1b6b1bd50b2d7475867ed
sexozavr <daniildiveev@mail.ru>
2023-03-18 23:03:37 +0300

    [MODIFY] implement file loading and iterating over parsed objects in CollectionHandler

--- 2nd_semester/java/lab4/app/src/main/java/ru/ifmo/se/lab4/handler/CollectionHandler.java
+++ 2nd_semester/java/lab4/app/src/main/java/ru/ifmo/se/lab4/handler/CollectionHandler.java
@@ -1,11 +1,14 @@
 package ru.ifmo.se.lab4.handler;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
 import ru.ifmo.se.lab4.entities.Route;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.FileInputStream;
 import java.io.StringReader;
 import java.time.LocalDateTime;
 import java.util.PriorityQueue;
@@ -16,19 +19,36 @@ public class CollectionHandler {
     private PriorityQueue<Route> collection;
 
     public CollectionHandler() throws Exception {
-        //TODO: implement loading collection from file
+        //Loading file and storing information in string
+        FileInputStream fis = new FileInputStream(pathToCollection);
         String xml = "";
+        int i;
 
+        while ((i=fis.read()) != -1) {
+            xml += (char) i;
+        }
+
+        //Parsing XML from string
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
         DocumentBuilder builder = factory.newDocumentBuilder();
         InputSource is = new InputSource(new StringReader(xml));
         Document doc = builder.parse(is);
+
+        NodeList routes = doc.getElementsByTagName("route");
+
+        //Iterating over parsed objects
+        for (i = 0; i < routes.getLength(); i++){
+            Node child = routes.item(i);
+            String name = child.getNodeName();
+            String contents = child.getTextContent();
+        }
     }
 
     public String info(){
         String output = "Collection " + collection.getClass().getSimpleName();
-        output += " containing " + collection.size() + " of object Route. ";
-        output += " Collection created on " + dateCreated.toString() + ".";
+        output += " containing " + collection.size() + " of object Route. \n";
+        output += " Collection created on " + dateCreated + ".\n";
+        output += " Collection stored at " + pathToCollection + ".\n";
 
         return output;
     }
