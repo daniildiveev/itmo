96d772bccaa5a1a5c3db49628b9310a3c53c928a
sexozavr <daniildiveev@mail.ru>
2023-03-18 22:21:09 +0300

    [MODIFY] implement constructor, info, getCollection and updateCollection methods for CollectionHandler'

--- 2nd_semester/java/lab4/app/src/main/java/ru/ifmo/se/lab4/handler/CollectionHandler.java
+++ 2nd_semester/java/lab4/app/src/main/java/ru/ifmo/se/lab4/handler/CollectionHandler.java
@@ -1,10 +1,42 @@
 package ru.ifmo.se.lab4.handler;
 
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
+import ru.ifmo.se.lab4.entities.Route;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.StringReader;
 import java.time.LocalDateTime;
+import java.util.PriorityQueue;
 
 public class CollectionHandler {
     private final LocalDateTime dateCreated = LocalDateTime.now();
-    private String pathToCollection = System.getenv("LABA5_SOURCE_FILE");
+    private final String pathToCollection = System.getenv("LABA5_SOURCE_FILE");
+    private PriorityQueue<Route> collection;
 
-    public CollectionHandler(){}
+    public CollectionHandler() throws Exception {
+        //TODO: implement loading collection from file
+        String xml = "";
+
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        InputSource is = new InputSource(new StringReader(xml));
+        Document doc = builder.parse(is);
+    }
+
+    public String info(){
+        String output = "Collection " + collection.getClass().getSimpleName();
+        output += " containing " + collection.size() + " of object Route. ";
+        output += " Collection created on " + dateCreated.toString() + ".";
+
+        return output;
+    }
+
+    void updateCollection(PriorityQueue<Route> collection){
+        this.collection = collection;
+    }
+    public PriorityQueue<Route> getCollection() {
+        return collection;
+    }
 }
