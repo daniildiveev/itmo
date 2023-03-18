c9c632e91da277cee2f3dd26595f6639e8e07491
sexozavr <daniildiveev@mail.ru>
2023-03-18 22:20:07 +0300

    [MODIFY] add info command handling, implement command execution

--- 2nd_semester/java/lab4/app/src/main/java/ru/ifmo/se/lab4/handler/CommandHandler.java
+++ 2nd_semester/java/lab4/app/src/main/java/ru/ifmo/se/lab4/handler/CommandHandler.java
@@ -1,9 +1,12 @@
 package ru.ifmo.se.lab4.handler;
 
 import ru.ifmo.se.lab4.commands.Command;
+import ru.ifmo.se.lab4.entities.Route;
+
+import java.util.PriorityQueue;
 
 public class CommandHandler{
-    public static void process(String rawInput){
+    public static void process(String rawInput, CollectionHandler collectionHandler){
         String[] args = rawInput.split("\\s+");
         String commandName = args[0].strip();
 
@@ -11,14 +14,20 @@ public class CommandHandler{
             System.exit(1);
         }
 
-        Command command = PackageParser.getCommand("ru.ifmo.se.lab4.commands", commandName, "Command");
-
-        if (command == null){
-            IOHandler.println("No such command: " + commandName);
+        else if (commandName.equals("info")){
+            IOHandler.println(collectionHandler.info());
         }
 
         else {
-            //TODO: implement command execution (CollectionHandler needed)
+            Command command = PackageParser.getCommand("ru.ifmo.se.lab4.commands", commandName, "Command");
+
+            if (command == null) {
+                IOHandler.println("No such command: " + commandName);
+            } else {
+                PriorityQueue<Route> collection = collectionHandler.getCollection();
+                command.execute(collection, args);
+                collectionHandler.updateCollection(collection);
+            }
         }
     }
 }
