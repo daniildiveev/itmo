072d86fa448e8017b288b51b9b079bba3c996325
sexozavr <daniildiveev@mail.ru>
2023-03-18 17:19:03 +0300

    [MODIFY] implement execute for help

--- 2nd_semester/java/lab4/gradle/app/src/main/java/ru/ifmo/se/lab4/commands/Help.java
+++ 2nd_semester/java/lab4/gradle/app/src/main/java/ru/ifmo/se/lab4/commands/Help.java
@@ -1,7 +1,11 @@
 package ru.ifmo.se.lab4.commands;
 
 import java.util.PriorityQueue;
+import java.util.Set;
+
 import ru.ifmo.se.lab4.entities.Route;
+import ru.ifmo.se.lab4.handler.PackageParser;
+
 public class Help implements Command{
     public String getName(){
         return "help";
@@ -11,6 +15,18 @@ public class Help implements Command{
         return getName() + "                            -- show information about available commands\n";
     }
     public void execute(PriorityQueue<Route> collection, String[] args){
-        System.out.println("Not implemented yet");
+        Set<Class> commands = PackageParser.parsePackage("ru.ifmo.se.lab4.commands", "Command");
+        String output = "";
+
+        for(Class klass: commands){
+            try{
+                Command command = (Command) klass.getConstructor().newInstance();
+                output +=  command.getDescription();
+            }
+            catch (Exception e){
+                System.out.println(e.getMessage());
+            }
+
+        }
     }
 }
