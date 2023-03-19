05831c3f3a085785f222060b80500a2a6db23921
sexozavr <daniildiveev@mail.ru>
2023-03-19 16:39:36 +0300

    [MODIFY] add xml regex matching and from file collection creation

--- 2nd_semester/java/lab4/app/src/main/java/ru/ifmo/se/lab4/handler/CollectionHandler.java
+++ 2nd_semester/java/lab4/app/src/main/java/ru/ifmo/se/lab4/handler/CollectionHandler.java
@@ -1,22 +1,19 @@
 package ru.ifmo.se.lab4.handler;
 
-import org.w3c.dom.Document;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import org.xml.sax.InputSource;
 import ru.ifmo.se.lab4.entities.Route;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.FileInputStream;
-import java.io.StringReader;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.PriorityQueue;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class CollectionHandler {
     private final LocalDateTime dateCreated = LocalDateTime.now();
     private final String pathToCollection = System.getenv("LABA5_SOURCE_FILE");
-    private PriorityQueue<Route> collection;
+    private PriorityQueue<Route> collection = new PriorityQueue<Route>();
 
     public CollectionHandler() throws Exception {
         //Loading file and storing information in string
@@ -28,27 +25,25 @@ public class CollectionHandler {
             xml += (char) i;
         }
 
-        //Parsing XML from string
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder builder = factory.newDocumentBuilder();
-        InputSource is = new InputSource(new StringReader(xml));
-        Document doc = builder.parse(is);
+        //Matching XML regex
+        List<String> routesXml = new ArrayList<String>();
+        Matcher m = Pattern.compile("<route(?:\sid=\"\\d+\")*>[\\s\\S]*?</route>").matcher(xml);
 
-        NodeList routes = doc.getElementsByTagName("route");
+        while (m.find()){
+            routesXml.add(m.group());
+        }
 
-        //Iterating over parsed objects
-        for (i = 0; i < routes.getLength(); i++){
-            Node child = routes.item(i);
-            String name = child.getNodeName();
-            String contents = child.getTextContent();
+        //Creating collection
+        for (String routeXml: routesXml){
+            this.collection.add(new Route(routeXml));
         }
     }
 
     public String info(){
         String output = "Collection " + collection.getClass().getSimpleName();
         output += " containing " + collection.size() + " of object Route. \n";
-        output += " Collection created on " + dateCreated + ".\n";
-        output += " Collection stored at " + pathToCollection + ".\n";
+        output += "Collection created on " + dateCreated + ".\n";
+        output += "Collection stored at " + pathToCollection + ".\n";
 
         return output;
     }
