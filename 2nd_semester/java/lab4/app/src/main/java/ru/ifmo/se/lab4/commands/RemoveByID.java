1cd607f09cca5955049e39116d7aed4e65f80895
sexozavr <daniildiveev@mail.ru>
2023-03-19 22:17:06 +0300

    [ADD] implement remove_by_id command

---
+++ 2nd_semester/java/lab4/app/src/main/java/ru/ifmo/se/lab4/commands/RemoveByID.java
@@ -0,0 +1,42 @@
+package ru.ifmo.se.lab4.commands;
+
+import ru.ifmo.se.lab4.entities.Route;
+import ru.ifmo.se.lab4.handler.CollectionHandler;
+import ru.ifmo.se.lab4.handler.IOHandler;
+
+import java.util.PriorityQueue;
+
+public class RemoveByID implements Command{
+    @Override
+    public String getName() {
+        return "remove_by_id";
+    }
+
+    @Override
+    public String getDescription() {
+        return " id                 -- remove element with respect to id\n";
+    }
+
+    @Override
+    public void execute(CollectionHandler collectionHandler, String[] args) {
+        PriorityQueue<Route> collection = collectionHandler.getCollection();
+        PriorityQueue<Route> new_collection = new PriorityQueue<>();
+
+        try{
+            int targetId = Integer.parseInt(args[1]);
+
+            for (Route r:collection){
+                int id = r.getId();
+
+                if (id != targetId){
+                    new_collection.add(r);
+                }
+            }
+
+            collectionHandler.updateCollection(new_collection);
+        }
+        catch (Exception e){
+            IOHandler.println("Invalid id provided");
+        }
+    }
+}
