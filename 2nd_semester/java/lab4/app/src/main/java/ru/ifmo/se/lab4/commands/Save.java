11a3a967db97cea857a5b4073cb846d0d2776d80
sexozavr <daniildiveev@mail.ru>
2023-03-19 17:55:33 +0300

    [MODIFY] fix save path in Save, change writing in file method, handle NoFile exception

--- 2nd_semester/java/lab4/app/src/main/java/ru/ifmo/se/lab4/commands/Save.java
+++ 2nd_semester/java/lab4/app/src/main/java/ru/ifmo/se/lab4/commands/Save.java
@@ -1,6 +1,7 @@
 package ru.ifmo.se.lab4.commands;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.PriorityQueue;
 import java.io.FileOutputStream;
 import ru.ifmo.se.lab4.entities.Route;
@@ -15,23 +16,25 @@ public class Save implements Command{
     }
 
     public void execute(PriorityQueue<Route> collection, String[] args) {
-        String savePath = args[0];
-        String output = "<routes>";
+        String savePath = System.getenv("LABA5_SOURCE_FILE");
+        String output = "";
 
         for (Route route : collection){
             output += route.toXml();
         }
 
-        output += "</routes>";
-
         try {
-            FileOutputStream fout = new FileOutputStream(savePath, true);
-            byte b[] = output.getBytes();
+            FileOutputStream fout = new FileOutputStream(savePath, false);
+            byte[] b = output.getBytes();
             fout.write(b);
             fout.close();
         }
 
-        catch(Exception e){
+        catch(FileNotFoundException e){
+            IOHandler.println("No such file:" + savePath);
+        }
+
+        catch (IOException e){
             IOHandler.println(e.getMessage());
         }
     }
