33d4dfdc454c4ed8349daac27beeb81a3d39592f
sexozavr <daniildiveev@mail.ru>
2023-03-19 22:32:35 +0300

    [MODIFY] delete info and exit commands handling in CommandHandler

--- 2nd_semester/java/lab4/app/src/main/java/ru/ifmo/se/lab4/handler/CommandHandler.java
+++ 2nd_semester/java/lab4/app/src/main/java/ru/ifmo/se/lab4/handler/CommandHandler.java
@@ -1,31 +1,18 @@
 package ru.ifmo.se.lab4.handler;
 
 import ru.ifmo.se.lab4.commands.Command;
-import ru.ifmo.se.lab4.entities.Route;
-
-import java.util.PriorityQueue;
 
 public class CommandHandler{
     public static void process(String rawInput, CollectionHandler collectionHandler){
         String[] args = rawInput.split("\\s+");
         String commandName = args[0].strip();
 
-        if (commandName.equals("exit")) {
-            System.exit(0);
-        }
+        Command command = PackageParser.getCommand("ru.ifmo.se.lab4.commands", commandName, "Command");
 
-        else if (commandName.equals("info")){
-            IOHandler.println(collectionHandler.info());
-        }
-
-        else {
-            Command command = PackageParser.getCommand("ru.ifmo.se.lab4.commands", commandName, "Command");
-
-            if (command == null) {
-                IOHandler.println("No such command: " + commandName);
-            } else {
-                command.execute(collectionHandler, args);
-            }
+        if (command == null) {
+            IOHandler.println("No such command: " + commandName);
+        } else {
+            command.execute(collectionHandler, args);
         }
     }
 }
