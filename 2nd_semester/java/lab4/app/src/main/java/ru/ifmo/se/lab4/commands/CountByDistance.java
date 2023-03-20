68be4b566f0f3b53e6b977a649706d34ebff8575
sexozavr <daniildiveev@mail.ru>
2023-03-21 01:36:50 +0300

    [MODIFY] handle parsing exception in count_by_distance

--- 2nd_semester/java/lab4/app/src/main/java/ru/ifmo/se/lab4/commands/CountByDistance.java
+++ 2nd_semester/java/lab4/app/src/main/java/ru/ifmo/se/lab4/commands/CountByDistance.java
@@ -4,6 +4,7 @@ import ru.ifmo.se.lab4.entities.Route;
 import ru.ifmo.se.lab4.handler.CollectionHandler;
 import ru.ifmo.se.lab4.handler.IOHandler;
 
+import java.io.IOException;
 import java.util.PriorityQueue;
 
 public class CountByDistance implements Command{
@@ -21,16 +22,21 @@ public class CountByDistance implements Command{
     public void execute(CollectionHandler collectionHandler, String [] args){
         PriorityQueue<Route> collection = collectionHandler.getCollection();
 
-        long distance = Long.parseLong(args[0]);
-        int counter = 0;
+        try{
+            long distance = Long.parseLong(args[1]);
+            int counter = 0;
 
-        for(Route r: collection){
-            if (r.getDistance() == distance){
-                counter++;
+            for(Route r: collection){
+                if (r.getDistance() == distance){
+                    counter++;
+                }
             }
-        }
 
-        String output = "Number of Routes with distance " + distance + ": " + counter;
-        IOHandler.println(output);
+            String output = "Number of Routes with distance " + distance + ": " + counter;
+            IOHandler.println(output);
+        }
+        catch (NumberFormatException e){
+            IOHandler.println("Invalid distance provided");
+        }
     }
 }
