00da4964f310b44cc8d0fe7f4adae8e9c13f2c26
sexozavr <daniildiveev@mail.ru>
2023-03-29 16:29:51 +0300

    [MODIFY] change primary key setting to default

--- 2nd_semester/db/lab0/fill_with_test_data.sql
+++ 2nd_semester/db/lab0/fill_with_test_data.sql
@@ -1,46 +1,46 @@
 INSERT INTO spheres_of_work VALUES
-    (1, 'связи с общественностью'),
-    (2, 'химическая промышленность'),
-    (3, 'IT');
+    (DEFAULT, 'связи с общественностью'),
+    (DEFAULT, 'химическая промышленность'),
+    (DEFAULT, 'IT');
 
 INSERT INTO education_level VALUES
-    (1, 'профессор'),
-    (2, 'студент'),
-    (3, 'школьник'),
-    (4, 'бакалавр');
+    (DEFAULT, 'профессор'),
+    (DEFAULT, 'студент'),
+    (DEFAULT, 'школьник'),
+    (DEFAULT, 'бакалавр');
 
 INSERT INTO profession VALUES
-    (1, 'ген подрядчик'),
-    (2, 'рабочий завода'),
-    (3, 'охранник'),
-    (4, 'помощник');
+    (DEFAULT, 'ген подрядчик'),
+    (DEFAULT, 'рабочий завода'),
+    (DEFAULT, 'охранник'),
+    (DEFAULT, 'помощник');
 
 INSERT INTO health_status VALUES
-    (1, 'здоровый'),
-    (2, 'мертв'),
-    (3, 'болеющий');
+    (DEFAULT, 'здоровый'),
+    (DEFAULT, 'мертв'),
+    (DEFAULT, 'болеющий');
 
 INSERT INTO location VALUES
-    (1, 'остров'),
-    (2, 'город'),
-    (3, 'пустыня'),
-    (4, 'завод');
+    (DEFAULT, 'остров'),
+    (DEFAULT, 'город'),
+    (DEFAULT, 'пустыня'),
+    (DEFAULT, 'завод');
 
 INSERT INTO person (id, name, location, age, sphere, physical_condition, profession, education_level) VALUES
-    (1, 'Хардинг', 2, 37, 1, 1, 1, 4),
-    (2, 'Оуэнз', 2, 34, 1, 2, 1, 4),
-    (3, 'Эд Реджис', 1, 40, 2, 1, 2, 4),
-    (4, 'Вадик', 3, 7, null, 1, null, 3),
-    (5, 'Костик', 1, 8, null, 1, null, 3);
+    (DEFAULT, 'Хардинг', 2, 37, 1, 1, 1, 4),
+    (DEFAULT, 'Оуэнз', 2, 34, 1, 2, 1, 4),
+    (DEFAULT, 'Эд Реджис', 1, 40, 2, 1, 2, 4),
+    (DEFAULT, 'Вадик', 3, 7, null, 1, null, 3),
+    (DEFAULT, 'Костик', 1, 8, null, 1, null, 3);
 
 INSERT INTO person_location VALUES
     (3, 1, make_interval(0, 7)),
     (2, 4, make_interval(0, 0, 0, 0, 1));
 
 INSERT INTO tasks (id, date, description) VALUES
-    (1, make_date(1999, 1, 1), 'Серьезное дело на острове'),
-    (2, make_date(1999, 2, 3), 'Уволить половину работников завода'),
-    (3, make_date(1999, 3, 3), 'Экскурсия с детьми');
+    (DEFAULT, make_date(1999, 1, 1), 'Серьезное дело на острове'),
+    (DEFAULT, make_date(1999, 2, 3), 'Уволить половину работников завода'),
+    (DEFAULT, make_date(1999, 3, 3), 'Экскурсия с детьми');
 
 INSERT INTO person_task VALUES
     (1, 1),
@@ -50,9 +50,9 @@ INSERT INTO person_task VALUES
     (3, 3);
 
 INSERT INTO problems VALUES
-    (1, 'Не понимать что ты профессионал!'),
-    (2, 'Приближающаеся наводнение'),
-    (3, 'Не хватает одного ребенка!');
+    (DEFAULT, 'Не понимать что ты профессионал!'),
+    (DEFAULT, 'Приближающаеся наводнение'),
+    (DEFAULT, 'Не хватает одного ребенка!');
 
 INSERT INTO problem_person VALUES
     (1, 1),
@@ -63,9 +63,9 @@ INSERT INTO problem_person VALUES
     (3, 2);
 
 INSERT INTO limbs VALUES
-    (1, 'голова'),
-    (2, 'нога'),
-    (3, 'рука');
+    (DEFAULT, 'голова'),
+    (DEFAULT, 'нога'),
+    (DEFAULT, 'рука');
 
 INSERT INTO limbs_person VALUES
     (4, 1),
