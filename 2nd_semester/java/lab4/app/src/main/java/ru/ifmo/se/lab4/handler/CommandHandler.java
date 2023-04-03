4c6df2be9c21ee4f628a332855c4f15d1d21b4a1
sexozavr <daniildiveev@mail.ru>
2023-04-03 21:49:31 +0300

    [MODIFY] add CommandWithElement proper handling

--- 2nd_semester/java/lab4/app/src/main/java/ru/ifmo/se/lab4/handler/CommandHandler.java
+++ 2nd_semester/java/lab4/app/src/main/java/ru/ifmo/se/lab4/handler/CommandHandler.java
@@ -1,22 +1,31 @@
 package ru.ifmo.se.lab4.handler;
 
 import ru.ifmo.se.lab4.commands.Command;
+import ru.ifmo.se.lab4.commands.CommandWithElement;
 
 public class CommandHandler{
-    public static void process(String rawInput, CollectionHandler collectionHandler){
+    public static void process(String rawInput, CollectionHandler collectionHandler, boolean fromFile){
         String[] args = rawInput.split("\\s+");
         String commandName = args[0].strip();
 
-        if (commandName.equals("")){
+        if (commandName.isEmpty()){
             IOHandler.println("Command cannot be empty!");
             return;
         }
 
-        Command command = PackageParser.getCommand("ru.ifmo.se.lab4.commands", commandName, "Command");
+        Command command = PackageParser.getCommand("ru.ifmo.se.lab4.commands",
+                                                   commandName, new String[]{"Command", "CommandWithElement"});
 
         if (command == null) {
             IOHandler.println("No such command: " + commandName);
-        } else {
+        }
+
+        else if(fromFile & command instanceof CommandWithElement){
+            CommandWithElement commandWithElement = (CommandWithElement) command;
+            commandWithElement.executeFromFile(collectionHandler, args);
+        }
+
+        else {
             command.execute(collectionHandler, args);
         }
     }
