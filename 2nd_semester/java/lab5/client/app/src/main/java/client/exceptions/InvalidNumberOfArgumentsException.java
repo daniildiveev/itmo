daa28beeb8d0ccb58c1ac0ef29febd5db20adac9
sexozavr <daniildiveev@mail.ru>
2023-04-23 01:59:50 +0300

    [ADD] add exceptions for client part lab5

---
+++ 2nd_semester/java/lab5/client/app/src/main/java/client/exceptions/InvalidNumberOfArgumentsException.java
@@ -0,0 +1,7 @@
+package client.exceptions;
+
+public class InvalidNumberOfArgumentsException extends Exception{
+    public InvalidNumberOfArgumentsException(String message){
+        super(message);
+    }
+}
---
+++ 2nd_semester/java/lab5/client/app/src/main/java/client/exceptions/InvalidParameterValueException.java
@@ -0,0 +1,7 @@
+package client.exceptions;
+
+public class InvalidParameterValueException extends Exception{
+    public InvalidParameterValueException(String message){
+        super(message);
+    }
+}
