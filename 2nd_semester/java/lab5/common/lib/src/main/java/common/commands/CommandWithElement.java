79440b241d477a1d99769c1407c4110e09fcd329
sexozavr <daniildiveev@mail.ru>
2023-05-04 17:32:04 +0300

    [MODIFY] implement setRoute in CommandWithElement

--- 2nd_semester/java/lab5/common/lib/src/main/java/common/commands/CommandWithElement.java
+++ 2nd_semester/java/lab5/common/lib/src/main/java/common/commands/CommandWithElement.java
@@ -1,5 +1,11 @@
 package common.commands;
 
+import common.entities.Route;
+
 public class CommandWithElement extends Command{
+    protected Route route;
     public void executeFromFile(){}
+    public void setRoute(Route r){
+        this.route = r;
+    }
 }
