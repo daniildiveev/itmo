059fc75920235bd71e20854625d591566177111d
sexozavr <daniildiveev@mail.ru>
2023-05-04 17:49:01 +0300

    [MODIFY] add route creation for command with element to CommandHandler

--- 2nd_semester/java/lab5/common/lib/src/main/java/common/handler/CommandHandler.java
+++ 2nd_semester/java/lab5/common/lib/src/main/java/common/handler/CommandHandler.java
@@ -1,10 +1,14 @@
 package common.handler;
 
 import common.commands.Command;
+import common.commands.CommandWithElement;
 import common.exceptions.InvalidCommandNameException;
+import common.entities.Route;
+
+import java.util.Arrays;
 
 public class CommandHandler{
-    public static Command process(String rawInput, CollectionHandler collectionHandler) throws InvalidCommandNameException{
+    public static Command process(String rawInput) throws InvalidCommandNameException{
         String[] args = rawInput.split("\\s+");
         String commandName = args[0].trim();
 
@@ -16,8 +20,15 @@ public class CommandHandler{
                 commandName, new String[]{"Command", "CommandWithElement"});
 
         if (command == null) {
-            throw new InvalidCommandNameException("no such command" + commandName);
+            throw new InvalidCommandNameException("no such command: " + commandName);
         }
+
+        if(command instanceof CommandWithElement){
+            Route r = new Route();
+            ((CommandWithElement) command).setRoute(r);
+        }
+
+        command.setArgs(Arrays.copyOfRange(args, 1, args.length));
 
         return command;
     }
