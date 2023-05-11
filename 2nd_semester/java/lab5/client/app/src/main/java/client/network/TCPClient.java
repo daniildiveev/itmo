ecb1f66e86c2a29f6e9959302d7bf541c06877b0
sexozavr <daniildiveev@mail.ru>
2023-05-11 15:31:58 +0300

    [ADD] implement show command in lab5 java

---
+++ 2nd_semester/java/lab5/client/app/src/main/java/client/network/TCPClient.java
@@ -0,0 +1,49 @@
+package client.network;
+
+import common.commands.Command;
+import common.handler.IOHandler;
+
+import java.io.*;
+import java.net.InetSocketAddress;
+import java.nio.Buffer;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.StandardCharsets;
+
+public class TCPClient {
+
+    private String host = "127.0.0.1";
+    private int port = 3333;
+    private SocketChannel clientSocket;
+
+    public boolean connectToServer() {
+        try {
+            this.clientSocket = SocketChannel.open(new InetSocketAddress(host, port));
+            return true;
+        } catch (IOException ioe) {
+            IOHandler.println("Ошибка при подключении: " + ioe.getMessage());
+            return false;
+        }
+    }
+
+    public void closeConnection() throws IOException {
+        this.clientSocket.close();
+    }
+
+    public boolean sendRequest(Command command) throws IOException {
+        if(connectToServer()){
+            ObjectOutput objectOutput = new ObjectOutputStream(this.clientSocket.socket().getOutputStream());
+            InputStream in = new BufferedInputStream(clientSocket.socket().getInputStream());
+
+            objectOutput.writeObject(command);
+
+            String str_in = new String(in.readAllBytes(), StandardCharsets.UTF_8);
+            IOHandler.print(str_in);
+            in.close();
+
+            objectOutput.close();
+            closeConnection();
+        }
+        return true;
+    }
+
+}
---
+++ 2nd_semester/java/lab5/common/lib/src/main/java/common/commands/Show.java
@@ -0,0 +1,35 @@
+package common.commands;
+
+import common.entities.Route;
+import common.handler.CollectionHandler;
+import common.handler.IOHandler;
+
+import java.io.PrintWriter;
+import java.util.PriorityQueue;
+
+public class Show extends Command {
+    @Override
+    public String getName() {
+        return "show";
+    }
+
+    @Override
+    public String getDescription() {
+        return getName() + "                            -- show all elements of collection in String representation\n";
+    }
+
+    @Override
+    public void execute(CollectionHandler collectionHandler, PrintWriter output) {
+        PriorityQueue<Route> collection = collectionHandler.getCollection();
+
+        if (collection.isEmpty()){
+            output.println("Collection is empty!");
+        }
+
+        for(Route r: collection){
+            output.println(r.toString());
+            output.println("");
+        }
+    }
+}
+
