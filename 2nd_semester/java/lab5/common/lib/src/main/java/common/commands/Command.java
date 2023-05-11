7c9e2c2cef3df19dc7fce9a000eb9042588a21d2
sexozavr <daniildiveev@mail.ru>
2023-05-11 15:33:11 +0300

    [MODIFY] add printWriter as an argument to command.execute, implement executionMessage method

--- 2nd_semester/java/lab5/common/lib/src/main/java/common/commands/Command.java
+++ 2nd_semester/java/lab5/common/lib/src/main/java/common/commands/Command.java
@@ -2,6 +2,7 @@ package common.commands;
 
 import common.handler.CollectionHandler;
 
+import java.io.PrintWriter;
 import java.io.Serializable;
 
 public abstract class Command implements Serializable {
@@ -13,12 +14,16 @@ public abstract class Command implements Serializable {
     public String getDescription(){
         return null;
     };
-    public void execute(CollectionHandler collectionHandler){};
+    public void execute(CollectionHandler collectionHandler, PrintWriter output){};
     public void setArgs(String[] args){
         this.args = args;
     }
 
     public static int getNumStringsToRead() {
         return numStringsToRead;
+    }
+
+    public void executionMessage(PrintWriter output){
+        output.println("Executing command " + getName());
     }
 }
