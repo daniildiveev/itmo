d7a34f703f146978fe95581bea20aa199586d685
sexozavr <daniildiveev@mail.ru>
2023-05-13 01:17:25 +0300

    [MODIFY] fix getName and getDescription in remove_lower

--- 2nd_semester/java/lab5/common/lib/src/main/java/common/commands/RemoveLower.java
+++ 2nd_semester/java/lab5/common/lib/src/main/java/common/commands/RemoveLower.java
@@ -7,24 +7,22 @@ import java.io.PrintWriter;
 import java.util.PriorityQueue;
 
 public class RemoveLower extends CommandWithElement{
-        @Override
-        public String getName() {
-            return "remove_greater";
-        }
-
-        @Override
-        public String getDescription() {
-            return getName() + " {element}        -- remove all elements from collection that is greater than given\n";
-        }
-
-        @Override
-        public void execute(CollectionHandler collectionHandler, PrintWriter output) {
-            PriorityQueue<Route> collection = collectionHandler.getCollection();
-
-            collection.stream()
-                    .filter(route -> this.route.compareTo(route) < 0)
-                    .forEach(collection::remove);
-        }
+    @Override
+    public String getName() {
+        return "remove_lower";
     }
 
+    @Override
+    public String getDescription() {
+        return getName() + " {element}        -- remove all elements from collection that is lower than given";
+    }
+
+    @Override
+    public void execute(CollectionHandler collectionHandler, PrintWriter output) {
+        PriorityQueue<Route> collection = collectionHandler.getCollection();
+
+        collection.stream()
+                .filter(route -> this.route.compareTo(route) < 0)
+                .forEach(collection::remove);
+    }
 }
