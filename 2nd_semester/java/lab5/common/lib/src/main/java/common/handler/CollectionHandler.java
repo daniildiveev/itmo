e1a4ce1de7ad899c3d2445ef0c858e22d117431c
sexozavr <daniildiveev@mail.ru>
2023-05-13 16:29:39 +0300

    [MODIFY] add createRouteFromXml to CollectionHandler

--- 2nd_semester/java/lab5/common/lib/src/main/java/common/handler/CollectionHandler.java
+++ 2nd_semester/java/lab5/common/lib/src/main/java/common/handler/CollectionHandler.java
@@ -1,11 +1,15 @@
 package common.handler;
 
+import common.commands.CommandWithElement;
 import common.entities.Route;
+import common.setter.RouteAutomaticFieldsSetter;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.xml.sax.InputSource;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStreamReader;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -16,7 +20,7 @@ import java.util.regex.Pattern;
 public class CollectionHandler {
     private final LocalDateTime dateCreated = LocalDateTime.now();
     private final String pathToCollection = System.getenv("LABA5_SOURCE_FILE");
-    private PriorityQueue<Route> collection = new PriorityQueue<Route>();
+    private PriorityQueue<Route> collection = new PriorityQueue<>();
 
     public CollectionHandler() throws Exception {
         //Loading file and storing information in string
@@ -50,7 +54,7 @@ public class CollectionHandler {
             //Creating collection
             for (String routeXml : routesXml) {
                 try {
-                    //this.collection.add(new Route(routeXml));
+                    this.collection.add(createRouteFromXml(routeXml));
                 } catch (Exception e) {
                     IOHandler.println(e.getMessage());
                     IOHandler.println("Skipping this Route...");
@@ -58,6 +62,59 @@ public class CollectionHandler {
             }
         }
     }
+
+    public Route createRouteFromXml(String sourceXml) throws Exception{
+        String[] args = new String[CommandWithElement.getNumStringsToRead()];
+        int i = 0;
+
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        InputSource is = new InputSource(new StringReader(sourceXml));
+        Document doc = builder.parse(is);
+
+        Element idElement = (Element) doc.getElementsByTagName("route").item(0);
+
+        String idValue = idElement.getAttribute("id");
+        RouteAutomaticFieldsSetter.validateId(idValue);
+
+        String datetime = doc.getElementsByTagName("creationDate")
+                .item(0)
+                .getTextContent()
+                .trim();
+        RouteAutomaticFieldsSetter.validateCreationDate(datetime);
+
+        String name = doc.getElementsByTagName("name").item(0).getTextContent().trim();
+        args[i++] = name;
+
+        //Parsing coordinates object
+        Element xmlElement = (Element) doc.getDocumentElement()
+                .getElementsByTagName("coordinates")
+                .item(0);
+
+        args[i++] = xmlElement.getElementsByTagName("x").item(0).getTextContent();
+        args[i++] = xmlElement.getElementsByTagName("y").item(0).getTextContent();
+
+        //Parsing location fields
+        for (String el : new String[] {"from", "to"}) {
+            xmlElement = (Element) doc.getDocumentElement()
+                    .getElementsByTagName(el)
+                    .item(0);
+
+            args[i++] = xmlElement.getElementsByTagName("x").item(0).getTextContent();
+            args[i++] = xmlElement.getElementsByTagName("y").item(0).getTextContent();
+            args[i++] = xmlElement.getElementsByTagName("z").item(0).getTextContent();
+            args[i++] = xmlElement.getElementsByTagName("name").item(0).getTextContent();
+        }
+
+        args[i++] = doc.getElementsByTagName("distance").item(0).getTextContent();
+
+        Route r = new Route(args);
+        r.setCreationDate(LocalDateTime.parse(datetime));
+        r.setId(Integer.parseInt(idValue));
+
+        return new Route(args);
+    }
+
 
     public String info(){
         String output = "Collection " + this.collection.getClass().getSimpleName();
