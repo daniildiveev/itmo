91c3f8d42450b139f2b7fe9decd2e58576081664
sexozavr <daniildiveev@mail.ru>
2023-05-13 16:26:47 +0300

    [MODIFY] add creationDate and id validators to RouteAutomaticFieldsSetter

--- 2nd_semester/java/lab5/common/lib/src/main/java/common/setter/RouteAutomaticFieldsSetter.java
+++ 2nd_semester/java/lab5/common/lib/src/main/java/common/setter/RouteAutomaticFieldsSetter.java
@@ -1,7 +1,9 @@
 package common.setter;
 
 import common.entities.Route;
+import common.exceptions.InvalidParameterValueException;
 import common.handler.CollectionHandler;
+import common.handler.IOHandler;
 
 import java.time.LocalDateTime;
 import java.util.*;
@@ -28,5 +30,35 @@ public class RouteAutomaticFieldsSetter {
 
     public static LocalDateTime generateTimestamp(){
         return LocalDateTime.now();
+    }
+
+    public static boolean validateId(String idString){
+        try{
+            int id = Integer.parseInt(idString);
+
+            if(id < 1){
+                throw new InvalidParameterValueException("Id must be greater than 1");
+            }
+
+            return true;
+        } catch (NumberFormatException e){
+            IOHandler.println("Cannot read id of integer type");
+        } catch (InvalidParameterValueException e){
+            IOHandler.println(e.getMessage());
+        }
+
+        return false;
+    }
+
+    public static boolean validateCreationDate(String creationDateString){
+        try{
+            LocalDateTime creationDate = LocalDateTime.parse(creationDateString);
+
+            return true;
+        } catch (Exception e){
+            IOHandler.println(e.getMessage());
+        }
+
+        return false;
     }
 }
