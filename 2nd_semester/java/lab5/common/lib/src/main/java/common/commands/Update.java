0f285e40e09f10160249f8fb1966effc77af5227
sexozavr <daniildiveev@mail.ru>
2023-05-13 15:22:18 +0300

    [ADD] implement update command

---
+++ 2nd_semester/java/lab5/common/lib/src/main/java/common/commands/Update.java
@@ -0,0 +1,44 @@
+package common.commands;
+
+import common.entities.Route;
+import common.handler.CollectionHandler;
+import common.setter.RouteAutomaticFieldsSetter;
+
+import java.io.PrintWriter;
+import java.util.Optional;
+import java.util.PriorityQueue;
+
+public class Update extends CommandWithElement{
+    @Override
+    public String getName() {
+        return "update";
+    }
+
+    @Override
+    public String getDescription() {
+        return getName() + " id {element}             -- update collection element with respect to id\n";
+    }
+
+    @Override
+    public void execute(CollectionHandler collectionHandler, PrintWriter output) {
+        try{
+            int idToChange = Integer.parseInt(this.args[0]);
+            PriorityQueue<Route> collection = collectionHandler.getCollection();
+
+            Optional<Route> route = collection.stream()
+                    .filter(r -> r.getId() == idToChange)
+                    .findFirst();
+
+            if(route.isPresent()){
+                collection.remove(route.get());
+                this.route.setId(route.get().getId());
+                this.route.setCreationDate(RouteAutomaticFieldsSetter.generateTimestamp());
+                collection.add(this.route);
+            } else {
+                output.println("Element with provided id not found in collection");
+            }
+        } catch (NumberFormatException e){
+            output.println("Invalid id provided");
+        }
+    }
+}
