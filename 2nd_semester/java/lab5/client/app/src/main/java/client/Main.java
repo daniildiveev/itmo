6c933f21b21c23f21d17cc5b7b4bd9bc09406f33
sexozavr <daniildiveev@mail.ru>
2023-05-31 03:08:09 +0300

    [MODIFY] minor lab5 client part main fixes

--- 2nd_semester/java/lab5/client/app/src/main/java/client/Main.java
+++ 2nd_semester/java/lab5/client/app/src/main/java/client/Main.java
@@ -1,26 +1,21 @@
 package client;
 
-import java.io.IOException;
-import java.util.List;
-import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
+import common.commands.Command;
 import common.commands.CommandWithElement;
 import common.commands.ExecuteScript;
 import common.commands.Exit;
 import common.entities.Route;
-import org.apache.log4j.PropertyConfigurator;
-
 import common.exceptions.InvalidCommandNameException;
-import common.handler.IOHandler;
-import client.network.TCPClient;
-import common.commands.Command;
 import common.handler.CommandHandler;
+import common.handler.IOHandler;
+import common.network.TCPClient;
+
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.logging.Logger;
 
 public class Main {
     public static void main(String[] args) {
-        //PropertyConfigurator.configure(Main.class.getClassLoader().getResource("log4j.properties"));
         Logger logger = Logger.getLogger("logger");
 
         Scanner scanner = new Scanner(System.in);
@@ -33,32 +28,21 @@ public class Main {
             try {
                 Command command = CommandHandler.process(input);
 
+                if (command instanceof Exit) System.exit(0);
+
                 if(command instanceof CommandWithElement){
                     Route r = new Route();
                     ((CommandWithElement) command).setRoute(r);
                 }
 
                 if(command instanceof ExecuteScript){
-                    List<Command> commands = ((ExecuteScript) command).retrieveCommands();
+                    ((ExecuteScript) command).retrieveCommands(client);
 
-                    if (commands != null){
-                            commands.forEach(commandObject -> {
-                                try{
-                                    client.sendRequest(commandObject);
-                                } catch (IOException e){
-                                    logger.log(Level.SEVERE, "Problem when sending data to server: " + e.getMessage(), e.getMessage());
-                            }
-                        });
-                    } else {
-                        IOHandler.println("Could not read commands from file");
-                    }
                 } else {
                     client.sendRequest(command);
                 }
-
-                if (command instanceof Exit) System.exit(0);
             } catch (InvalidCommandNameException|IOException e){
-                IOHandler.println(e.getMessage());
+                IOHandler.println(e.getClass().getSimpleName() + ": " + e.getMessage());
             }
         }
     }
