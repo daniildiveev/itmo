418d023c50bb1a6c1ed3e1def5177808c3ad96e9
sexozavr <daniildiveev@mail.ru>
2023-06-12 18:27:34 +0300

    [MODIFY] add setUser method

--- 2nd_semester/java/lab6/common/src/main/java/common/commands/authentication/AuthenticationCommand.java
+++ 2nd_semester/java/lab6/common/src/main/java/common/commands/authentication/AuthenticationCommand.java
@@ -1,10 +1,17 @@
 package common.commands.authentication;
 
 import common.commands.Command;
+import common.network.User;
 
 public abstract class AuthenticationCommand implements Command {
+    private User user;
+
     @Override
     public String getName() {
         return "authentication_command";
+    }
+
+    public void setUser(User user){
+        this.user = user;
     }
 }
