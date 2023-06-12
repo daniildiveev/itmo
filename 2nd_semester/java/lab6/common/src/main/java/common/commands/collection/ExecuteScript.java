d7065a436444f7379ecc32f76f9f07fb06510283
sexozavr <daniildiveev@mail.ru>
2023-06-12 18:31:49 +0300

    [MODIFY] fix ExecuteScript so it sends request object instead of commands

--- 2nd_semester/java/lab6/common/src/main/java/common/commands/collection/ExecuteScript.java
+++ 2nd_semester/java/lab6/common/src/main/java/common/commands/collection/ExecuteScript.java
@@ -1,11 +1,14 @@
 package common.commands.collection;
 
+import common.commands.Command;
 import common.entities.Route;
 import common.exceptions.InvalidCommandNameException;
 import common.handler.CommandHandler;
 import common.handler.FileHandler;
 import common.handler.IOHandler;
+import common.network.Request;
 import common.network.TCPClient;
+import common.network.User;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -28,7 +31,7 @@ public class ExecuteScript extends CollectionCommand {
         return getName() + " file_name        -- read and execute script from provided file";
     }
 
-    public void retrieveCommands(TCPClient client) {
+    public void retrieveCommands(TCPClient client, User user) {
         List<CollectionCommand> commands = new ArrayList<>();
 
         try {
@@ -54,40 +57,45 @@ public class ExecuteScript extends CollectionCommand {
                         rawInput = scanner.nextLine();
 
                         try {
-                            CollectionCommand command = CommandHandler.process(rawInput);
+                            Command command = CommandHandler.process(rawInput);
 
-                            if (command instanceof CommandWithElement) {
-                                String[] routeArgs = new String[CommandWithElement.getNumStringsToRead()];
+                            if(command instanceof CollectionCommand){
+                                if (command instanceof CommandWithElement) {
+                                    String[] routeArgs = new String[CommandWithElement.getNumStringsToRead()];
 
-                                boolean ok = true;
+                                    boolean ok = true;
 
-                                for (int i = 0; i < CommandWithElement.getNumStringsToRead(); i++) {
-                                    if (i < CommandWithElement.getNumStringsToRead() - 1 && !scanner.hasNextLine()) {
-                                        ok = false;
-                                        break;
-                                    } else {
-                                        routeArgs[i] = scanner.nextLine();
+                                    for (int i = 0; i < CommandWithElement.getNumStringsToRead(); i++) {
+                                        if (i < CommandWithElement.getNumStringsToRead() - 1 && !scanner.hasNextLine()) {
+                                            ok = false;
+                                            break;
+                                        } else {
+                                            routeArgs[i] = scanner.nextLine();
+                                        }
                                     }
-                                }
 
-                                if (ok) {
-                                    try {
-                                        Route r = new Route(routeArgs);
+                                    if (ok) {
+                                        try {
+                                            Route r = new Route(routeArgs);
+                                            System.out.println("Sending command" + command.getName());
+                                            ((CommandWithElement) command).setRoute(r);
+                                        } catch (Exception e) {
+                                            IOHandler.println(e.getMessage());
+                                        }
+                                    }
+                                } else {
+                                    if (command instanceof ExecuteScript) {
+                                        ((ExecuteScript) command).retrieveCommands(client, user);
+                                    } else {
                                         System.out.println("Sending command" + command.getName());
-                                        ((CommandWithElement) command).setRoute(r);
-                                        client.sendRequest(command);
-                                    } catch (Exception e) {
-                                        IOHandler.println(e.getMessage());
                                     }
                                 }
                             } else {
-                                if (command instanceof ExecuteScript) {
-                                    ((ExecuteScript) command).retrieveCommands(client);
-                                } else {
-                                    System.out.println("Sending command" + command.getName());
-                                    client.sendRequest(command);
-                                }
+                                IOHandler.println("Non-collection commands not allowed in script");
                             }
+
+                            Request request = new Request(command, user);
+                            client.sendRequest(request);
                         } catch (InvalidCommandNameException e) {
                             IOHandler.println("Skipping command " + rawInput + " since it is not present in package");
                         }
