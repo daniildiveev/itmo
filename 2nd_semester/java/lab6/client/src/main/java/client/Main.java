2d3ee5b3b92222ba914a30fae05e841f423b5d88
sexozavr <daniildiveev@mail.ru>
2023-06-18 17:56:17 +0300

    [MODIFY] add authentication to client main

--- 2nd_semester/java/lab6/client/src/main/java/client/Main.java
+++ 2nd_semester/java/lab6/client/src/main/java/client/Main.java
@@ -1,23 +1,23 @@
 package client;
 
 import common.commands.Command;
-import common.commands.CommandWithElement;
-import common.commands.ExecuteScript;
-import common.commands.Exit;
+import common.commands.authentication.AuthenticationCommand;
+import common.commands.collection.CollectionCommand;
+import common.commands.collection.CommandWithElement;
+import common.commands.collection.ExecuteScript;
+import common.commands.collection.Exit;
 import common.entities.Route;
-import common.exceptions.InvalidCommandNameException;
+import common.exceptions.*;
 import common.handler.CommandHandler;
 import common.handler.IOHandler;
 import common.network.TCPClient;
+import common.network.User;
 
 import java.io.IOException;
 import java.util.Scanner;
-import java.util.logging.Logger;
 
 public class Main {
     public static void main(String[] args) {
-        Logger logger = Logger.getLogger("logger");
-
         Scanner scanner = new Scanner(System.in);
         TCPClient client = new TCPClient();
 
@@ -28,21 +28,30 @@ public class Main {
             try {
                 Command command = CommandHandler.process(input);
 
-                if (command instanceof Exit) System.exit(0);
+                if((command instanceof CollectionCommand) && client.getUser() != null){
+                    if (command instanceof Exit) System.exit(0);
 
-                if(command instanceof CommandWithElement){
-                    Route r = new Route();
-                    ((CommandWithElement) command).setRoute(r);
-                }
+                    if (command instanceof CommandWithElement) {
+                        Route r = new Route();
+                        ((CommandWithElement) command).setRoute(r);
+                    }
 
-                if(command instanceof ExecuteScript){
-                    ((ExecuteScript) command).retrieveCommands(client);
+                    if (command instanceof ExecuteScript) {
+                        ((ExecuteScript) command).retrieveCommands(client);
 
-                } else {
+                    } else {
+                        client.sendRequest(command);
+                    }
+                } else if (command instanceof AuthenticationCommand){
+                    ((AuthenticationCommand) command).setUser(new User());
                     client.sendRequest(command);
+                } else {
+                    throw new NotLoggedInException("not logged in yet");
                 }
             } catch (InvalidCommandNameException|IOException e){
                 IOHandler.println(e.getClass().getSimpleName() + ": " + e.getMessage());
+            } catch (NotLoggedInException e){
+                IOHandler.println("You haven't logged in yet! Type login or register!");
             }
         }
     }
