e7f6172103b691be778d07e9ad4118812d53581e
sexozavr <daniildiveev@mail.ru>
2023-06-21 00:48:27 +0300

    [RENAME] rename RemoveCommand to CommandWithUser

--- 2nd_semester/java/lab6/common/src/main/java/common/commands/collection/CommandWithUser.java
+++ 2nd_semester/java/lab6/common/src/main/java/common/commands/collection/CommandWithUser.java
@@ -3,7 +3,7 @@ package common.commands.collection;
 import common.commands.Command;
 import common.network.User;
 
-public abstract class RemoveCommand implements Command {
+public abstract class CommandWithUser implements Command {
     private User user;
 
     public void setUser(User user) {
