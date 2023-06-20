d9ae9b7c28a976fd2b481a1e68a5b3e89062bb4b
sexozavr <daniildiveev@mail.ru>
2023-06-20 23:58:18 +0300

    [MODIFY] add user field and getUser method to CommandWithElement

--- 2nd_semester/java/lab6/common/src/main/java/common/commands/collection/CommandWithElement.java
+++ 2nd_semester/java/lab6/common/src/main/java/common/commands/collection/CommandWithElement.java
@@ -1,9 +1,11 @@
 package common.commands.collection;
 
 import common.entities.Route;
+import common.network.User;
 
 public abstract class CommandWithElement extends CollectionCommand {
     protected Route route;
+    protected User user;
 
     public void executeFromFile() {
     }
@@ -14,5 +16,9 @@ public abstract class CommandWithElement extends CollectionCommand {
 
     public void setRoute(Route r) {
         this.route = r;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
     }
 }
