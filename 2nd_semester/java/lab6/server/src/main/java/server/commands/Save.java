559195e6ad737b690716cd2d767d64ff939a8f7a
sexozavr <daniildiveev@mail.ru>
2023-06-22 00:49:30 +0300

    [MODIFY] remove old save, leave blank class

--- 2nd_semester/java/lab6/server/src/main/java/server/commands/Save.java
+++ 2nd_semester/java/lab6/server/src/main/java/server/commands/Save.java
@@ -1,14 +1,7 @@
 package server.commands;
 
 import common.commands.collection.CollectionCommand;
-import common.entities.Route;
 import common.handler.CollectionHandler;
-import common.handler.IOHandler;
-
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.util.PriorityQueue;
 
 public class Save extends CollectionCommand {
     @Override
@@ -17,36 +10,6 @@ public class Save extends CollectionCommand {
     }
 
     public void save(CollectionHandler collectionHandler) {
-        PriorityQueue<Route> collection = collectionHandler.getCollection();
 
-        String savePath = System.getenv("LABA5_SOURCE_FILE");
-
-        if(savePath == null){
-            System.out.println("There is no environment variable with collection file path");
-            return;
-        }
-
-        String xmlOutput = "<routes>\n";
-
-        for (Route route : collection){
-            xmlOutput += route.toXml();
-        }
-
-        xmlOutput += "</routes>";
-
-        try {
-            FileOutputStream fout = new FileOutputStream(savePath, false);
-            byte[] b = xmlOutput.getBytes();
-            fout.write(b);
-            fout.close();
-        }
-
-        catch(FileNotFoundException e){
-            IOHandler.println("No such file:" + savePath);
-        }
-
-        catch (IOException e){
-            IOHandler.println(e.getMessage());
-        }
     }
 }
