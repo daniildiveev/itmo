94fb2ede9d6dcd1735592811459a28e79877ef6f
sexozavr <daniildiveev@mail.ru>
2023-06-22 15:55:35 +0300

    [MODIFY] add checkIfNull method, rename checkIfNull -> checkIfNullString

--- 2nd_semester/java/lab6/common/src/main/java/common/validator/Validator.java
+++ 2nd_semester/java/lab6/common/src/main/java/common/validator/Validator.java
@@ -4,14 +4,16 @@ import common.exceptions.InvalidParameterValueException;
 
 public abstract class Validator {
     public static void checkEmptinessOfParameter(String parameter, String parameterName) throws InvalidParameterValueException{
-        if (parameter.trim().isEmpty() || checkIfNull(parameter)){
+        if (parameter.trim().isEmpty() || checkIfNullString(parameter)){
             throw new InvalidParameterValueException(parameterName + " parameter of" + getEntityName() +" cannot be null");
         }
     }
 
-    public static boolean checkIfNull(String parameter){
+    public static boolean checkIfNullString(String parameter){
         return parameter.trim().equals("null");
     }
+
+    public static boolean checkIfNull(Object o){return o == null;}
 
     public static String getEntityName(){
         return "None";
