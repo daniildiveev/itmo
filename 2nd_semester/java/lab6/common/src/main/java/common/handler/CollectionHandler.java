66ad6c76438d48db86a57278dde7ba4094b52998
sexozavr <daniildiveev@mail.ru>
2023-06-22 15:51:59 +0300

    [MODIFY] add ReadWriteLock to CollectionHandler

--- 2nd_semester/java/lab6/common/src/main/java/common/handler/CollectionHandler.java
+++ 2nd_semester/java/lab6/common/src/main/java/common/handler/CollectionHandler.java
@@ -4,29 +4,51 @@ import common.entities.Route;
 
 import java.time.LocalDateTime;
 import java.util.PriorityQueue;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class CollectionHandler {
     private final LocalDateTime dateCreated = LocalDateTime.now();
     private final String pathToCollection = System.getenv("LABA5_SOURCE_FILE");
     private PriorityQueue<Route> collection = new PriorityQueue<>();
+    private ReadWriteLock collectionLock = new ReentrantReadWriteLock();
 
     public CollectionHandler() throws Exception {
         this.collection = DBHandler.loadCollectionToMemory();
     }
 
     public String info(){
-        String output = "Collection " + this.collection.getClass().getSimpleName();
-        output += " containing " + this.collection.size() + " of object Route. \n";
-        output += "Collection created on " + dateCreated + ".\n";
-        output += "Collection stored at " + pathToCollection + ".\n";
+        collectionLock.readLock().lock();
 
-        return output;
+        try{
+            String output = "Collection " + this.collection.getClass().getSimpleName();
+            output += " containing " + this.collection.size() + " of object Route. \n";
+            output += "Collection created on " + dateCreated + ".\n";
+            output += "Collection stored at " + pathToCollection + ".\n";
+
+            return output;
+        } finally {
+            collectionLock.readLock().unlock();
+        }
     }
 
     public void updateCollection(PriorityQueue<Route> collection){
-        this.collection = collection;
+        collectionLock.writeLock().lock();
+
+        try{
+            this.collection = collection;
+        } finally {
+            collectionLock.writeLock().unlock();
+        }
+
     }
     public PriorityQueue<Route> getCollection() {
-        return this.collection;
+        collectionLock.readLock().lock();
+
+        try{
+            return this.collection;
+        } finally {
+            collectionLock.readLock().unlock();
+        }
     }
 }
